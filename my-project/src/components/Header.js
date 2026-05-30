c6b09d6b979b3a69e14f3a46826a1d0c8eef8728
akupaloheimo <aku.paloheimo@hotmail.com>
2026-05-30 23:40:13 +0300

    mobile-navbar-changes

--- my-project/src/components/Header.js
+++ my-project/src/components/Header.js
@@ -1,33 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Resume from "../assets/CV.pdf";
 
 const Header = () => {
-  return (
-    <header className="flex justify-between items-center px-9 py-6 bg-primary text-white fixed w-full z-20 shadow-lg">
-      <a href="/" className="text-xl">
-        Aku Paloheimo
-      </a>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-      <nav className="hidden md:flex space-x-6 py-3, itmes-center">
-        {["About", "Experience", "Portfolio"].map((item) => (
-          <a
-            key={item}
-            href={`/${item.toLowerCase()}`}
-            className="font-semibold text-white text-xl hover:text-accent transition-colors"
-          >
-            {item}
-          </a>
-        ))}
-        <a
-          className="font-semibold text-white text-xl hover:text-accent transition-colors"
-          href={Resume}
-          id="pdfLink"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CV
+  return (
+    <header className="bg-primary text-white fixed w-full z-20 shadow-lg">
+      <div className="flex justify-between items-center px-4 md:px-9 py-6">
+        <a href="/" className="text-xl">
+          Aku Paloheimo
         </a>
-      </nav>
+
+        <nav className="hidden md:flex space-x-6 items-center">
+          {["About", "Experience", "Portfolio"].map((item) => (
+            <a
+              key={item}
+              href={`/${item.toLowerCase()}`}
+              className="font-semibold text-xl hover:text-accent transition-colors"
+            >
+              {item}
+            </a>
+          ))}
+
+          <a
+            href={Resume}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-semibold text-xl hover:text-accent transition-colors"
+          >
+            CV
+          </a>
+        </nav>
+
+        <button
+          className="md:hidden text-3xl "
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          ☰
+        </button>
+      </div>
+
+      {menuOpen && (
+        <nav className="md:hidden flex flex-col px-4 pb-4 space-y-4 bg-primary">
+          {["About", "Experience", "Portfolio"].map((item) => (
+            <a
+              key={item}
+              href={`/${item.toLowerCase()}`}
+              className="font-semibold text-xl hover:text-accent transition-colors"
+            >
+              {item}
+            </a>
+          ))}
+
+          <a
+            href={Resume}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-semibold text-xl hover:text-accent transition-colors"
+          >
+            CV
+          </a>
+        </nav>
+      )}
     </header>
   );
 };
